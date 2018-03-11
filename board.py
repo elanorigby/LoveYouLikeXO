73e946faeda9a69297e393a83bd0507478053e07
Sharla <bob@bob.bob>
2018-03-11 09:04:01 +0000

    rough draft of board

---
+++ board.py
@@ -0,0 +1,14 @@
+# just draw the board 
+
+wall = "+---+---+---+"
+cells = "| {} | {} | {} |"
+
+def draw_board():
+    print(wall)
+    print(cells)
+    print(wall)
+    print(cells)
+    print(wall)
+
+
+draw_board()
