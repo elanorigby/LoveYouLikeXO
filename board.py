2ba3633c7888831e31bee611d2a4abf8d2f085a0
Sharla <bob@bob.bob>
2018-03-11 09:11:34 +0000

    begin board class, start drying up drawing board

--- board.py
+++ board.py
@@ -1,14 +1,20 @@
 # just draw the board 
 
-wall = "+---+---+---+"
-cells = "| {} | {} | {} |"
+class Board:
+    def __init__(self):
+        self.wall = "+---+---+---+"
+        self.cells = "| {} | {} | {} |"
 
-def draw_board():
-    print(wall)
-    print(cells)
-    print(wall)
-    print(cells)
-    print(wall)
+    def part(self):
+        print(self.wall)
+        print(self.cells)
 
+    def draw(self):
+        self.part()
+        self.part()
+        self.part()
+        print(self.wall)
 
-draw_board()
+board = Board()
+board.draw()
+
