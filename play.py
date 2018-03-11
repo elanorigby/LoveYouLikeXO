23e6f3527d508947cebe371e29586d410b567911
Sharla <bob@bob.bob>
2018-03-11 10:26:03 +0000

    program stops when board is full. 2 hours over.

--- play.py
+++ play.py
@@ -2,6 +2,7 @@ import pdb
 from board import Board
 
 board = Board()
+moves = 0 
 
 print("Welcome to the world's premier Tick Tack Toe experience!")
 board.draw()
@@ -13,6 +14,14 @@ if player == 'X':
 elif player == 'O':
     AI = 'X'
 
-print("player", player, "AI", AI)
+# print("player", player, "AI", AI)
+
+# game loop zoen
+while moves <  9:
+    move = int(input("Where would you like to move? "))
+    moves += 1
+    board.draw(move, player)
+
+print("The board is full. The game is over")
 
 
