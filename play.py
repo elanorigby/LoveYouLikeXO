298f0e3c65b5d2e99410733850d7b4e38a99eb12
giulio <giulio@gpf.localdomain>
2018-03-12 21:34:02 +0000

    if they choose something other than X or O they are politely told to try again

--- play.py
+++ play.py
@@ -6,7 +6,15 @@ board = Board()
 print("Welcome to the world's premier Tick Tack Toe experience!")
 board.draw()
 
-player = input("Would you like to play as X or O? ").upper()
+def get_player():
+    player = input("Would you like to play as X or O? ").upper()
+    if player not in "XO":
+        print("Sorry, that's not an option.")
+        get_player()
+    else:
+        return player
+
+player = get_player()
 
 if player == 'X':
     AI = 'O'
@@ -32,12 +40,6 @@ moves = []
 
 while len(moves) <  9:
     get_move(moves)
-#    move = int(input("Where would you like to move? "))
-#    if move in moves:
-#        print("That spot is taken.")
-#    else:
-#        moves.append(move)
-#        board.draw(move, player) 
     
 print("The board is full. The game is over")
 
